24674341567c8a25cfd808b287698a54e51736bf
gooma519 <gooma519@gmail.com>
2023-12-18 18:45:22 +0900

    chore : pr 확인

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='ko'>
-      <body className='font-[Pretendard]'>{children}</body>
+      <body className='font-pre'>{children}</body>
     </html>
   );
 }
