815a835398d044e3655cd1c7bc28bce840cb4d20
sooyeong <sooyeong@sooyeongui-MacBookPro.local>
2023-12-19 00:40:11 +0900

    [#26] feat : Add Component

---
+++ src/app/_component/Chip/Add/index.tsx
@@ -0,0 +1,19 @@
+import addIcon from '@/public/icons/add_icon.svg';
+import Image from 'next/image';
+
+interface AddProps {
+  size: 'large' | 'small';
+}
+
+function Add({ size }: AddProps) {
+  const divSize = size === 'small' ? 'w-[1.25rem] h-[1.25rem]' : 'w-[1.375rem] h-[1.375rem]';
+  const imageSize = size === 'small' ? 14.545 : 16;
+
+  return (
+    <div className={`flex ${divSize} shrink-0 items-center justify-center rounded-[0.25rem] bg-violet8 p-[0.1875rem]`}>
+      <Image width={imageSize} height={imageSize} src={addIcon.src} alt='추가' />
+    </div>
+  );
+}
+
+export default Add;
