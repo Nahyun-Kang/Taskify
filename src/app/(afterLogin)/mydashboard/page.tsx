5a6d3ae6cd4879d7adf7e6c30db8f2ade1c49743
Nahyun <tarea202001@gmail.com>
2023-12-21 02:23:44 +0900

    [#46] feat : Header pathname별 css 스타일 지정, mydashboard 경로 페이지 생성

---
+++ src/app/(afterLogin)/mydashboard/page.tsx
@@ -0,0 +1,11 @@
+import Header from '../_component/Header/Header';
+import SideMenu from '../_component/SideMenu/SideMenu';
+
+export default function MyPage() {
+  return (
+    <>
+      <SideMenu />
+      <Header />
+    </>
+  );
+}
