5eaa7ee26ebd2c77d5cf5f011c54347f167f358e
sooyeong <sooyeong@sooyeongui-MacBookPro.local>
2023-12-21 01:20:32 +0900

    [#50] feat : AddColumn Button Component

---
+++ src/app/_component/Button/AddColumn/index.tsx
@@ -0,0 +1,36 @@
+import Add from '@/src/app/_component/Chip/Add';
+interface AddColumnProps {
+  screen: 'desktop' | 'mobile' | 'tablet';
+  onClick: () => void;
+}
+
+export default function AddColumn({ screen = 'desktop', onClick }: AddColumnProps) {
+  const screens = {
+    desktop: {
+      width: 'w-[22.125rem]',
+      height: 'h-[4.375rem]',
+      fontSize: 'text-[1.125rem]',
+    },
+    mobile: {
+      width: 'w-[17.75rem]',
+      height: 'h-[3.75rem]',
+      fontSize: 'text-[1rem]',
+    },
+    tablet: {
+      width: 'w-[34rem]',
+      height: 'h-[4.375rem]',
+      fontSize: 'text-[1.125rem]',
+    },
+  };
+  const { width, height, fontSize } = screens[screen];
+  const iconSize = screen === 'mobile' ? 'small' : 'large';
+  return (
+    <button
+      className={`flex ${width} ${height} items-center justify-center gap-[0.75rem] rounded-[0.25rem] bg-white ${fontSize} border border-gray30 font-bold text-black80`}
+      onClick={onClick}
+    >
+      새로운 컬럼 추가하기
+      <Add size={iconSize} />
+    </button>
+  );
+}
