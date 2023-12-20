a7bb4b2be5cf84f5c71eefb304c108996e567f88
sooyeong <sooyeong@sooyeongui-MacBookPro.local>
2023-12-20 10:49:16 +0900

    [#27] feat : Cancel Button Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+'use client';
+import Cancel from './_component/Button/Cancel';
+import Input from './_component/Button/Input';
 export default function Home() {
+  const handleClick = () => {
+    console.log('click ');
+  };
   return (
     <main>
       <p>랜딩</p>
+      <Input size='large' onClick={handleClick} />
+      <Cancel size='small' onClick={handleClick} />
     </main>
   );
 }
