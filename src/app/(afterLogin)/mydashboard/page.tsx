724b602da6114af105b0c2410a975ac4cbcde540
Nahyun <tarea202001@gmail.com>
2023-12-21 16:53:26 +0900

    [#46] fix : SideMenu 파일 배럴 방식으로 변경

--- src/app/(afterLogin)/mydashboard/page.tsx
+++ src/app/(afterLogin)/mydashboard/page.tsx
@@ -1,5 +1,5 @@
 import Header from '../_component/Header';
-import SideMenu from '../_component/SideMenu/SideMenu';
+import SideMenu from '../_component/SideMenu';
 
 export default function MyPage() {
   return (
