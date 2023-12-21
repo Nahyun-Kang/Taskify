52dfa8df3347097ac7b58423a9bfbea3e73903c7
Nahyun <tarea202001@gmail.com>
2023-12-21 16:32:33 +0900

    [#46] feat : 프로필 영역 추가, 반응형 수정

--- src/app/(afterLogin)/_component/Header/index.tsx
+++ src/app/(afterLogin)/_component/Header/index.tsx
@@ -5,6 +5,7 @@ import Crown from '@/src/app/_component/Icons/Crown';
 import HeaderButton from './HeaderButton';
 import add from '@/public/images/add_box_icon.svg';
 import manage from '@/public/images/manage_icon.svg';
+import ProfileCollection from '../ProfileImgCollection';
 
 const DUMMY = {
   folder: '강나현의 대시보드',
@@ -23,25 +24,29 @@ export default function Header() {
   return (
     <div className='relative'>
       <div className='fixed left-0 right-0 top-0 h-[4.375rem] border-b-[.0625rem] bg-white'>
-        <div className='mx-auto my-0 flex h-full w-full items-center justify-between'>
+        <div className=' flex h-full items-center justify-between'>
           {/* 헤더영역 왼쪽 */}
-          <div className='ml-[5.6875rem] justify-end md:ml-[12.5rem] lg:ml-[21.25rem]'>
+          <div className='ml-0 justify-end md:ml-[12.5rem] lg:ml-[21.25rem]'>
             <div className='flex items-center gap-2'>
               <div className={`text-black30 text-xl font-bold ${titleClass}`}>{folderName}</div>
               <Crown className={`hidden ${crownClass}`} />
             </div>
           </div>
           {/* 헤더영역 오른쪽 */}
-          <div className='grow-1 flex'>
+          <div className='flex'>
             {!isMyDashboard && (
               <div className='flex gap-[.375rem] md:gap-4'>
                 <HeaderButton imageSrc={manage}>관리</HeaderButton>
                 <HeaderButton imageSrc={add}>초대하기</HeaderButton>
               </div>
             )}
-            {!isMyDashboard && <div className='ml-4 md:ml-4'>프로필 영역</div>}
             {!isMyDashboard && (
-              <div className='ml-4 mr-3 h-[2.375rem] w-0 rounded-md border-[.0625rem] stroke-gray30 stroke-1 md:ml-8 md:mr-6 lg:ml-10 lg:mr-8'></div>
+              <div className='ml-3 mr-[3.75rem] md:ml-6 md:mr-[5.25rem] lg:ml-8 lg:mr-[9.5rem]'>
+                <ProfileCollection />
+              </div>
+            )}
+            {!isMyDashboard && (
+              <div className=' mr-3 h-[2.375rem] w-0 rounded-md border-[.0625rem] stroke-gray30 stroke-1 md:mr-6 lg:mr-8'></div>
             )}
             <div className='relative mr-3 flex items-center gap-3 md:mr-10 lg:mr-20'>
               <div className='flex items-center justify-center'>
--- src/app/(afterLogin)/mydashboard/page.tsx
+++ src/app/(afterLogin)/mydashboard/page.tsx
@@ -1,4 +1,4 @@
-import Header from '../_component/Header/Header';
+import Header from '../_component/Header';
 import SideMenu from '../_component/SideMenu/SideMenu';
 
 export default function MyPage() {
