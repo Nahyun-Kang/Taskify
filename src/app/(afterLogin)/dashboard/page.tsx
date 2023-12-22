d6b923fb562b71a384b38394e74af2bb72d29848
Nahyun <tarea202001@gmail.com>
2023-12-22 15:43:48 +0900

    [#47] fix: header 모바일 반응형 왼쪽 영역 margin 수정

--- src/app/(afterLogin)/dashboard/page.tsx
+++ src/app/(afterLogin)/dashboard/page.tsx
@@ -1,6 +1,11 @@
+import Header from '../_component/Header';
+import SideMenu from '../_component/SideMenu';
+
 export default function Dashboard() {
   return (
     <div>
+      <SideMenu />
+      <Header />
       <h1>username</h1>
     </div>
   );
