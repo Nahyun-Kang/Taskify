f61e1f9154da09b2bce6d5e2a57fe16b66e860ef
sooyeong <sooyeong@sooyeongui-MacBookPro.local>
2023-12-22 18:47:38 +0900

    [#57] refactor : Confirm Component title prop 추가

--- src/app/_component/Button/Confirm/index.tsx
+++ src/app/_component/Button/Confirm/index.tsx
@@ -1,8 +1,9 @@
 interface ConfirmProps {
   size: 'large' | 'small';
   onClick: () => void;
+  title: string;
 }
-export default function Confirm({ size, onClick }: ConfirmProps) {
+export default function Confirm({ size, onClick, title = '확인' }: ConfirmProps) {
   const sizes = {
     large: { width: 'w-[7.5rem]', height: 'h-[3rem]', paddingY: 'py-[0.875rem]', paddingX: 'px-[2.875rem]' },
     small: { width: 'w-[8.625rem]', height: 'h-[2.625rem]', paddingY: 'py-[0.75rem]', paddingX: 'px-[3.5rem]' },
@@ -13,7 +14,7 @@ export default function Confirm({ size, onClick }: ConfirmProps) {
       className={`flex items-center justify-center rounded-[0.5rem]  bg-violet ${paddingX} ${paddingY} ${height} ${width} text-[0.8125rem] text-white`}
       onClick={onClick}
     >
-      확인
+      {title}
     </button>
   );
 }
