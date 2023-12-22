86b13be9380520d3a2342ed95521a22c729bedf5
Nahyun <tarea202001@gmail.com>
2023-12-22 16:20:47 +0900

    [#47] fix : mydashboard 페이지, dashboard 페이지 테스트용 코드 삭제

--- src/app/(afterLogin)/dashboard/page.tsx
+++ src/app/(afterLogin)/dashboard/page.tsx
@@ -1,14 +1,7 @@
-import Header from '../_component/Header';
-import InvitationDashboard from '../_component/InvitationDashboard';
-import SideMenu from '../_component/SideMenu';
-
 export default function Dashboard() {
   return (
     <div>
-      <SideMenu />
-      <Header />
-      <InvitationDashboard />
-      <h1>username</h1>
+      <h1>dashboard</h1>
     </div>
   );
 }
--- src/app/(afterLogin)/mydashboard/page.tsx
+++ src/app/(afterLogin)/mydashboard/page.tsx
@@ -1,11 +1,7 @@
-import Header from '../_component/Header';
-import SideMenu from '../_component/SideMenu';
-
 export default function MyPage() {
   return (
-    <>
-      <SideMenu />
-      <Header />
-    </>
+    <div>
+      <h1>myDashboard</h1>
+    </div>
   );
 }
