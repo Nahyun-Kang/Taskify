e9ab7f44a65211d58bc63c9d852c6befdd09f71e
Nahyun <tarea202001@gmail.com>
2023-12-25 23:47:58 +0900

    fix : Sign 버튼 사이즈 free로 수정

--- src/app/(beforeLogin)/signup/page.tsx
+++ src/app/(beforeLogin)/signup/page.tsx
@@ -34,7 +34,7 @@ export default function SignUp() {
           ></input>
           <label className='text-base font-normal'>이용약관에 동의합니다</label>
         </div>
-        <Sign size='small' isActive={false} onClick={handleClick} content='회원가입' />
+        <Sign size='free' isActive={false} onClick={handleClick} content='회원가입' />
       </form>
     </AuthLayout>
   );
