92d7cb9f6c7cdd72c682d6135b5a074546aadf3c
aowjarkwk <bulls1rose@naver.com>
2023-12-25 20:42:48 +0900

    [#61] 메인 브랜치 머지 후 사용되는 버튼의 screen을 free로 변경

--- src/app/(afterLogin)/_component/DashboardList/index.tsx
+++ src/app/(afterLogin)/_component/DashboardList/index.tsx
@@ -18,13 +18,13 @@ interface DashBoardListProps {
 export default function DashboardList({ dashBoards, totalPages }: DashBoardListProps) {
   return (
     <div className='flex w-full flex-col gap-6 bg-gray10'>
-      <div className='flex flex-col gap-2 md:gap-3 '>
-        <div className='grid grid-flow-row gap-2 md:grid-cols-2 md:gap-3 lg:grid-cols-3'>
-          <AddDashboard screen='mobile' onClick={() => {}} />
+      <div className='flex flex-col gap-2 md:gap-3'>
+        <div className='grid auto-rows-[3.625rem] gap-2 md:auto-rows-[4.25rem] md:grid-cols-2 md:gap-3 lg:auto-rows-[4.375rem]'>
+          <AddDashboard screen='free' onClick={() => {}} />
           {dashBoards.map((dashboard) => (
             <CardDashboard
               key={dashboard.id}
-              screen='mobile'
+              screen='free'
               onClick={() => {}}
               title={dashboard.title}
               createdByMe={dashboard.createdByMe}
@@ -32,8 +32,8 @@ export default function DashboardList({ dashBoards, totalPages }: DashBoardListP
             />
           ))}
         </div>
-        <div className='ml-auto flex items-center gap-3 text-[0.75rem]'>
-          <span>{totalPages} 페이지 중 1</span>
+        <div className='ml-auto grid auto-cols-[4.5rem] grid-flow-col items-center gap-3 text-[0.75rem] md:auto-cols-[5rem]'>
+          <span className='text-3 md:text-[0.875rem]'>{totalPages} 페이지 중 1</span>
           <PageNation
             size='small'
             isActiveBack={true}
