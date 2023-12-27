7614877da355a7f58a19db93ba74cc92a03401bd
Nahyun <tarea202001@gmail.com>
2023-12-27 15:42:33 +0900

    [#91] feat : 회원가입 태그 InputField -> InputForm으로 교체

--- src/app/(beforeLogin)/signup/page.tsx
+++ src/app/(beforeLogin)/signup/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import AuthLayout from '@/src/app/(beforeLogin)/_component/Auth/AuthLayout';
-import InputField from '@/src/app/_component/Input/InputField';
+import InputForm from '../../_component/InputForm';
 import Sign from '@/src/app/_component/Button/Sign';
 import { AUTH_MESSAGE } from '../_constants/auth';
 
@@ -10,32 +10,35 @@ export default function SignUp() {
   const handleSubmit = () => {};
   return (
     <AuthLayout message={AUTH_MESSAGE.signUp}>
-      <form onSubmit={handleSubmit} className='w-full'>
-        <div className='mb-[16px]'>
-          <InputField.EmailInput labelText='이메일' placeholder='이메일을 입력해 주세요' id='email' />
-        </div>
-        <div className='mb-[16px]'>
-          <InputField.EmailInput labelText='닉네임' placeholder='닉네임을 입력해 주세요' id='nickname' />
-        </div>
-        <div className='mb-[20px]'>
-          <InputField.PasswordInput labelText='비밀번호' placeholder='비밀번호를 입력해 주세요' id='password' />
-        </div>
-        <div className='mb-[20px]'>
-          <InputField.PasswordInput
-            labelText='비밀번호 확인'
-            placeholder='비밀번호를 한번 더 입력해 주세요'
-            id='password_check'
-          />
-        </div>
-        <div className='mb-[1.3125rem] flex items-center gap-2'>
-          <input
-            type='checkbox'
-            className={`h-[1.25rem] w-[1.25rem] appearance-none rounded border border-gray30 bg-contain checked:bg-[url('/images/custom-checkbox-icon.svg')] focus:outline-none`}
-          ></input>
-          <label className='text-base font-normal'>이용약관에 동의합니다</label>
-        </div>
-        <Sign type='submit' size='free' isActive={false} onClick={handleSubmit} content='회원가입' />
-      </form>
+      <div className='w-full'>
+        <InputForm onSubmit={handleSubmit}>
+          <div className='mb-[16px]'>
+            <InputForm.EmailInput label='이메일' placeholder='이메일을 입력해 주세요' id='email' />
+          </div>
+          <div className='mb-[16px]'>
+            <InputForm.TextInput label='닉네임' placeholder='닉네임을 입력해 주세요' id='nickname' />
+          </div>
+          <div className='mb-[20px]'>
+            <InputForm.PasswordInput label='비밀번호' placeholder='비밀번호를 입력해 주세요' id='password' />
+          </div>
+          <div className='mb-[20px]'>
+            <InputForm.PasswordCheck
+              label='비밀번호 확인'
+              placeholder='비밀번호를 한번 더 입력해 주세요'
+              id='password_check'
+              passwordId='password'
+            />
+          </div>
+          <div className='mb-[1.3125rem] flex items-center gap-2'>
+            <input
+              type='checkbox'
+              className={`h-[1.25rem] w-[1.25rem] appearance-none rounded border border-gray30 bg-contain checked:bg-[url('/images/custom-checkbox-icon.svg')] focus:outline-none`}
+            ></input>
+            <label className='text-base font-normal'>이용약관에 동의합니다</label>
+          </div>
+          <Sign type='submit' size='free' isActive={false} onClick={handleSubmit} content='회원가입' />
+        </InputForm>
+      </div>
     </AuthLayout>
   );
 }
