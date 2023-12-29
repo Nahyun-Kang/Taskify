34e14132ef54fdb1330775c049d12f8d31eec230
Nahyun <tarea202001@gmail.com>
2023-12-29 14:02:18 +0900

    [#91] : 회원가입 라우터 수정 완료

--- src/app/_hook/useRenderModal.tsx
+++ src/app/_hook/useRenderModal.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { FieldValues, SubmitHandler } from 'react-hook-form';
-import { useRecoilState } from 'recoil';
+import { useSetRecoilState } from 'recoil';
 
 import returnModal from '../_util/returnModal';
 import { ToDoCardDetailProps } from '../_component/modal/toDoCard';
@@ -19,7 +19,7 @@ interface CallModalType {
 // 특정 모달 컴포넌트가 담겨져있는 state와 모달호출함수를 리턴하는 커스텀 훅
 export default function useRenderModal(): [React.ReactElement | null, CallModalType] {
   const [modalType, setModalType] = useState<React.ReactElement | null>(null);
-  const [recoilModalType, setRecoilModalType] = useRecoilState(modalTypeState);
+  const setRecoilModalType = useSetRecoilState(modalTypeState);
 
   const callModal: CallModalType = (condition) => {
     const newReturnModal = returnModal({ ...condition, setModalType });
