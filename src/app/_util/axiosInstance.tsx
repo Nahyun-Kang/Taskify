5e295df55169e7bf18f0bb1c4bef52b7ac1ea903
Nahyun <tarea202001@gmail.com>
2023-12-30 20:38:35 +0900

    [#110] fix: 복구

--- src/app/_util/axiosInstance.tsx
+++ src/app/_util/axiosInstance.tsx
@@ -1,17 +1,8 @@
 import axios from 'axios';
-import { UserDataType } from '../_constant/type';
-
-const userDataString = localStorage.getItem('taskifyUserData');
-let accessToken = '';
-
-if (userDataString) {
-  const userData: UserDataType = JSON.parse(userDataString);
-  accessToken = userData.accessToken;
-}
 
 export const axiosInstance = axios.create({
   baseURL: 'https://sp-taskify-api.vercel.app/1-3/',
   headers: {
-    Authorization: `Bearer ${accessToken}`,
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzEsInRlYW1JZCI6IjEtMyIsImlhdCI6MTcwMjk4MjAyMiwiaXNzIjoic3AtdGFza2lmeSJ9.CyJw1VGMNUVnP97QL8coPmhfCeaBZkMHZDU1KjOyAyo`,
   },
 });
