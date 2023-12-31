b524d67c3ace68425f0ae32c677fc078a581ac58
Nahyun <tarea202001@gmail.com>
2023-12-31 18:05:23 +0900

    [#112] feat : axiosInstance 토큰 설정- 로컬스토리지

--- src/app/_util/axiosInstance.tsx
+++ src/app/_util/axiosInstance.tsx
@@ -1,8 +1,26 @@
 import axios from 'axios';
+import { UserDataType } from '../_constant/type';
+
+const getAccessToken = () => {
+  let userDatas;
+  let accessToken = '';
+  if (typeof window !== 'undefined') {
+    userDatas = localStorage.getItem('taskifyUserData');
+  }
+
+  if (userDatas) {
+    const userData: UserDataType = JSON.parse(userDatas);
+    console.log(userData);
+    accessToken = userData.accessToken;
+    console.log(accessToken);
+    return accessToken;
+  }
+};
 
 export const axiosInstance = axios.create({
   baseURL: 'https://sp-taskify-api.vercel.app/1-3/',
   headers: {
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzEsInRlYW1JZCI6IjEtMyIsImlhdCI6MTcwMjk4MjAyMiwiaXNzIjoic3AtdGFza2lmeSJ9.CyJw1VGMNUVnP97QL8coPmhfCeaBZkMHZDU1KjOyAyo`,
+    Authorization: `Bearer ${getAccessToken()}`,
+    'Cache-Control': 'no-cache',
   },
 });
