4864a300c1190ce08853d3bc0cce3cba1012041a
Nahyun-Kang <tarea202001@gmail.com>
2024-01-01 17:56:24 +0900

    [#111] fix : myboard 테스트 코드 삭제

--- src/app/(afterLogin)/myboard/page.tsx
+++ src/app/(afterLogin)/myboard/page.tsx
@@ -1,26 +1,9 @@
 'use client';
-import { useEffect, useTransition } from 'react';
-import { useRouter } from 'next/navigation';
 
 import DashboardList from '@/src/app/(afterLogin)/_component/DashboardList';
 import InvitationDashboard from '@/src/app/(afterLogin)/_component/InvitationDashboard';
 
 export default function MyBoard() {
-  const [isPending, startTransition] = useTransition();
-  const router = useRouter();
-
-  const handleRefresh = () => {
-    if (!isPending) {
-      startTransition(() => {
-        router.refresh();
-      });
-    }
-  };
-
-  useEffect(() => {
-    handleRefresh();
-  }, []);
-
   return (
     <div className='flex flex-1 bg-gray10'>
       <div className='mt-[4.3125rem] flex max-w-[63.875rem] flex-1 flex-col gap-11 p-6 px-10 md:p-10'>
