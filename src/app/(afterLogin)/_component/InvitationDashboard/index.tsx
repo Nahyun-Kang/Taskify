0a47ad42449a423039e9c9a1e5060e0e2b8a4e06
aowjarkwk <bulls1rose@naver.com>
2024-01-01 13:29:28 +0900

    [#120] 초대받은 대시보드 무한스크롤 & 초대 수락/거절 기능 구현

--- src/app/(afterLogin)/_component/InvitationDashboard/index.tsx
+++ src/app/(afterLogin)/_component/InvitationDashboard/index.tsx
@@ -1,30 +1,59 @@
 'use client';
 
-import { ChangeEvent, useCallback, useEffect, useState } from 'react';
-import InvitationList from './InvitationList';
-import MagnifyingGlass from '../Icons/MagnifyingGlass';
-import NoInvitation from './NoInvitation';
+import MagnifyingGlass from '@/src/app/(afterLogin)/_component/Icons/MagnifyingGlass';
+import InvitationList from '@/src/app/(afterLogin)/_component/InvitationDashboard/InvitationList';
+import NoInvitation from '@/src/app/(afterLogin)/_component/InvitationDashboard/NoInvitation';
+import { Invitations } from '@/src/app/(afterLogin)/_constant/type';
+import { putInvitation } from '@/src/app/_api/Dashboards';
+import useInfiniteScroll from '@/src/app/_hook/useInfiniteScroll';
+import { dashboardState } from '@/src/app/_recoil/dashboardAtom';
 import { axiosInstance } from '@/src/app/_util/axiosInstance';
+import { ChangeEvent, useCallback, useRef, useState } from 'react';
+import { useSetRecoilState } from 'recoil';
 
 export default function InvitationDashboard() {
-  const [invitations, setInvitations] = useState([]);
-  const getInvitations = useCallback(async () => {
-    const { data } = await axiosInstance.get('invitations');
-    setInvitations(data.invitations);
-  }, []);
+  const setDashboardData = useSetRecoilState(dashboardState);
+  const [invitations, setInvitations] = useState<Invitations[]>([]);
+  const [cursorId, setCursorId] = useState('');
 
   const [value, setValue] = useState('');
+
+  const target = useRef<HTMLDivElement>(null);
+
+  const getInvitations = useCallback(async () => {
+    if (cursorId === null) return;
+    const query = cursorId ? `&cursorId=${cursorId}` : '';
+    const { data } = await axiosInstance.get(`invitations?size=6${query}`);
+    setInvitations((prev) => [...prev, ...data.invitations]);
+    setCursorId(data.cursorId || null);
+  }, [cursorId]);
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
 
-  useEffect(() => {
-    getInvitations();
-  }, [getInvitations]);
+  const onIntersect: IntersectionObserverCallback = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.intersectionRatio > 0) {
+        getInvitations();
+      }
+    });
+  };
+
+  const handleInvitation = async (invitationId: number, accepted: boolean) => {
+    const newDashboard = await putInvitation(invitationId, accepted);
+    setInvitations(invitations.filter((invitation) => invitation.id !== invitationId));
+    if (accepted) {
+      setDashboardData((prev) => {
+        return { ...prev, dashboards: [newDashboard, ...prev.dashboards] };
+      });
+    }
+  };
+
+  useInfiniteScroll({ target, onIntersect: onIntersect, size: cursorId });
 
   return (
-    <div className='rounded-lg border-none bg-white px-[1.75rem] pb-[.0625rem] pt-8 '>
+    <div className='rounded-lg border-none bg-white px-[1.75rem] pb-[.0625rem] pt-8'>
       <h2 className='mb-5 text-[1.25rem] font-bold text-black80 md:text-[1.5rem]'>초대받은 대시보드</h2>
       {invitations.length !== 0 ? (
         <div>
@@ -36,11 +65,12 @@ export default function InvitationDashboard() {
               className='w-full text-[.875rem] focus:outline-none md:text-base'
             ></input>
           </div>
-          <InvitationList value={value} list={invitations} />
+          <InvitationList value={value} list={invitations} handleInvitation={handleInvitation} />
         </div>
       ) : (
         <NoInvitation />
       )}
+      {cursorId !== null && <div ref={target}></div>}
     </div>
   );
 }
