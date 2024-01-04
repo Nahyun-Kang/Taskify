8d1f943568c1c501cc24fc84a0c97f6cdb8b3ef2
aowjarkwk <bulls1rose@naver.com>
2024-01-04 15:18:02 +0900

    [QA] 태블릿 이하 대시보드 기본높이 제거

--- src/app/(afterLogin)/dashboard/[dashboardId]/page.tsx
+++ src/app/(afterLogin)/dashboard/[dashboardId]/page.tsx
@@ -85,7 +85,7 @@ export default function DashBoard({ params }: { params: { dashboardId: string } 
                   style={{
                     backgroundColor: snapshot.isDraggingOver ? '#F1EFFD' : '#FAFAFA',
                   }}
-                  className='border-gray-20 hide-scrollbar flex flex-1 flex-col border-b bg-gray10 lg:h-full lg:min-w-[22.125rem] lg:flex-col lg:overflow-scroll lg:border-b-0 lg:border-r'
+                  className='border-gray-20 hide-scrollbar flex flex-col border-b bg-gray10 lg:h-full lg:min-w-[22.125rem] lg:flex-col lg:overflow-scroll lg:border-b-0 lg:border-r'
                 >
                   <CardList key={column.id + 'col'} id={column.id} title={column.title} boardId={params.dashboardId} />
                   {provided.placeholder}
