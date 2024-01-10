744b6b2834d789c7639ef2453272b67a1466681b
고민혁 <yeppi9801@gmail.com>
2024-01-10 19:29:38 +0900

    feat: 케밥 컴포넌트 적용

--- src/app/_component/modal/todo/detail/_component/iconButton/index.tsx
+++ src/app/_component/modal/todo/detail/_component/iconButton/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 import Image from 'next/image';
-import kebab from '@/public/icons/kebab.svg';
+import Kebab from '@/src/app/_component/Icons/Kebab';
 import close from '@/public/icons/close_icon.svg';
 import { useSetRecoilState, useRecoilState } from 'recoil';
 import {
@@ -43,7 +43,7 @@ export default function IconButton({ handleKebab, cardId }: DetailIconButtonProp
   return (
     <div className='flex items-center gap-1 sm:right-[0.75rem] sm:top-[0.75rem] md:right-[1.75rem] md:top-[2rem] md:gap-[1.5rem]'>
       <span onClick={handleKebab} className=' relative h-[1.75rem] w-[1.75rem] '>
-        {kebab && <Image src={kebab} alt='케밥' fill priority />}
+        <Kebab />
         {isOpenPopOver ? (
           <div className='absolute right-2 top-full z-10 h-[5.125rem] w-[5.8125rem] rounded-[0.375rem] border border-[#d9d9d9]  bg-white p-[0.375rem] shadow-md '>
             <p
